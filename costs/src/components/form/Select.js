14c32697e64557c5d6618d8a5a5467791d8edaa9
pedro-fmm <pedrofmm13@gmail.com>
2022-11-09 11:31:35 -0300

    json-server api and select options

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -7,6 +7,9 @@ function Select({text, name, options, handleOnChange, value}) {
             <label htmlFor={name}>{text}: </label>
             <select name={name} id={name}>
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.name}>{option.name}</option>
+                ))}
             </select>
         </div>
     );
